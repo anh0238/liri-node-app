aeb5c1ef95f9d8138b4ec05d8e7effa8f383609e
ahend <anh0238@gmail.com>
2019-01-02 01:54:44 -0600

    updates to liri.js

--- liri.js
+++ liri.js
@@ -2,13 +2,13 @@
 require("dotenv").config();
 
 var keys = require("./keys");
-var spotify = new spotify(keys.spotify);
+
 
 var request = require("request");
 
-var moment = require("moment");
+//var moment = require("moment");
 
-var fs = require("fs");
+//var fs = require("fs");
 
 var spotify = new Spotify(keys.spotify);
 
@@ -47,14 +47,10 @@ var getSong = function(song) {
     } else {
         console.log("Song Information: ");
 
-        spotify.search(
-            {
-                type: "track",
-                query: song
-            },
-            function(error, data) {
-                if (error) {
-                    console.log(error);
+        spotify.search({ type: "track", query: song },
+            function(err, data) {
+                if (err) {
+                    return console.log(err);
                 }
 
                 var information = data.tracks.items;
@@ -71,13 +67,49 @@ var getSong = function(song) {
 
 // movie-this
 // node liri.js movie-this '<movie name here>
+var getMovie = function(movie) {
 
+    var queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=full&tomatoes=true&apikey=trilogy";
 
+    request(queryURL, function(error, response, body) {
+        var data = JSON.parse(body);
+
+        console.log("Title: " + data.Title);
+        console.log("Year: " + data.Year);
+        console.log("IMDB Rating: " + data.imdbRating);
+        console.log("Rotten Tomatoes Rating: " + data.Ratings[1].Value);
+        console.log("Country: " + data.Country);
+        console.log("Language: " + data.Language);
+        console.log("Plot: " + data.Plot);
+        console.log("Actors: " + data.Actors);
+    });
+};
 
 // do-what-it-says
 // node liri.js do-what-it-says
 
+var pick = function(caseData, functionData) {
+    switch (caseData) {
+    case "concert-this":
+      getBand(functionData);
+      break;
+    case "spotify-this-song":
+      getSong(functionData);
+      break;
+    case "movie-this":
+      getMovie(functionData);
+      break;
+/*     case "do-what-it-says":
+      doWhatItSays();
+      break; */
+    default:
+      console.log("LIRI doesn't know that");
+    }
+  };
 
+var runThis = function(argOne, argTwo) {
+    pick(argOne, argTwo);
+  };
 
-
+runThis(process.argv[2], process.argv.slice(3).join(" "));
 
